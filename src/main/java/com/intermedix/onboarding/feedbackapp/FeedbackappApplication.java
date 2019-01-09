b94db2746a0065e112ee0a6bf9349fcb8c613e1e
Vaidas Nargėlas <vaidas.nargelas@intermedix.com>
2019-01-09 09:25:52 +0200

    Create stub spring boot application that connects to mysql database

---
+++ src/main/java/com/intermedix/onboarding/feedbackapp/FeedbackappApplication.java
@@ -0,0 +1,14 @@
+package com.intermedix.onboarding.feedbackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackappApplication.class, args);
+	}
+
+}
+
